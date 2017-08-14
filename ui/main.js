034104043683d2c16620294fcfab054b81656ef8
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2017-08-15 04:30:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,13 +14,13 @@ button.onclick = function() {
     var request = new XMLHttpRequest();
   //capture the response and store 
   request.onreadystatechange == function() {
-      if(request.readyState)===XMLHttpRequest.DONE) {
+      if(request.readyState === XMLHttpRequest.DONE) {
           if(request.status==200){
            var counter=request.responseText;
            var span = document.getElementById("count");
            span.innerHTML=counter.toString();
           }
-      };
+      }
       // make the request 
       request.open('GET','http://himanshubeniwal015.imad.hasura-app.io/',true);
       request.send(null);
