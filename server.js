5de3226d4281ba2e52936a84ae88cc266d51c5b2
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2017-08-15 04:01:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,9 +69,18 @@ var htmlTemplate=
 `;
 return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter=0;
+app.get('/counter', function (req,res) {
+   counter=counter+1;
+   res.send(counter.toString());
+    
+});
+
 app.get('/:articlesName', function (req,res){
     //article name will be article one
     // articles [articlesname] == {}content object for article one 
