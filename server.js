0f212cd2565a57952f77933acd26beb936488bcf
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2017-08-15 02:51:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/ui/style.css', function (req, res) {
 
 
 // if request made to javascript file 
-app.get('/ui/main.css', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 // request made to image madi
