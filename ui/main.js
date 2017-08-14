a8ccba8b62597c8982ed5e488bfc02ff737b957b
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2017-08-15 03:07:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //code to change the content of page 
 var element= document.getElementById("tobechange");
 element.innerHTML= " this is changed text ";
+
+
+// move the image to right
+var pic= document.getElementById('image');
+image.onClick = function () {
+  image.style.marginLeft='100px';
+};
