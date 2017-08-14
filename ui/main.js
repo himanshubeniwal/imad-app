9f5476d2edec7817319bf921ad5e5def6cf695d1
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2017-08-15 03:08:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ element.innerHTML= " this is changed text ";
 
 // move the image to right
 var pic= document.getElementById('image');
-image.onClick = function () {
-  image.style.marginLeft='100px';
+pic.onClick = function () {
+  pic.style.marginLeft='100px';
 };
