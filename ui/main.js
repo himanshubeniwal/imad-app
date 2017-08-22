c11b05b0d7aca0cf83b6d8738ba9f572934d69aa
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2017-08-23 00:48:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ element.innerHTML= " this is changed text ";
 
 */ 
 
-var counter=0
+var counter=0;
 var button = document.getElementById("counter");
 button.onclick = function() {
     // create a Request to counter endpoint 
