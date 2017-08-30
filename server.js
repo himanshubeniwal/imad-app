b25f67eb43e1e990efdc21496b42fa3c7206e9eb
Himanshu Beniwal <himanshubeniwal015@gmail.com>
2017-08-31 01:19:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,15 +123,16 @@ app.post('/create-user', function( req,res) {
 app.post('/login', function (req,res) {
     var username= req.body.username;
     var password = req.body.password;
-    pool.query('SELECT * FROM "user" WHERE username= $1', [username], function (err, result) {
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
         if(err) {
             res.status(500).send(err.toString());
             
         }else {
-            if(result.rows.length===0) {
-            res.send(403).send('Username/password is invalid');
+            if(result.rows.length === 0) {
+            res.send(403).send('username/password is invalid');
             }
             else {
+                //match the password
                 var dbString = result.rows[0].password;
                 var salt = dbString.split('$')[2];
                 var hashedPassword = hash(password, salt); 
@@ -139,6 +140,7 @@ app.post('/login', function (req,res) {
                 if (hashedPassword === dbString) {
                     res.send('Credentials correct!!!! ');
                     
+                    //set the session
                 } else {
                     res.send(403).send('Username/password is invalid');
                 }
